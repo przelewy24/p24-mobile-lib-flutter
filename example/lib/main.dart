49d54d9927c7d2b9065d6b21043728803d4ddc63
Marek Latuszek <marek.latuszek@dialcom24.pl>
2020-09-29 12:49:50 +0200

    library file version 3.5.2

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:p24_sdk/p24_sdk.dart';
 import 'package:p24_sdk_example/main_page.dart';
 
 void main() {
 
-	P24SdkVersion.value()
-		.then((value) => print("Sdk version: $value"));
 
 	runApp(
 		MaterialApp(
